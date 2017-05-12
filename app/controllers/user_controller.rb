a0c7af23f7f5a32a3308796cb818e601d0362372
Hima Chitalia <hima_chhag@yahoo.com>
2017-05-12 13:37:37 +0000

    New error handing tools

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -16,12 +16,11 @@ class UserController < ApplicationController
     else
       user = User.create(username: params[:username], password: params[:password], password_confirmation: params[:confirm_password], email: params[:email])
       unless user.valid?
-        return erb :'users/password_error' if user.errors[:password_confirmation].any?
+        return erb :'users/password_confirmation_error' if user.errors[:password_confirmation].any?
         return erb :'users/username_error' if user.errors[:username].any?
         return erb :'users/email_error' if user.errors[:email].any?
       end
       session[:user_id] = user.id
-      binding.pry
       redirect to '/tips'
     end
   end
@@ -40,9 +39,8 @@ class UserController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect to "#{user.username.downcase}/tips"
-    else
-      redirect to '/signup'
     end
+      erb :'users/login_error'
   end
 
   get '/logout' do
